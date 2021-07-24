c1d8b64271a4c9926dbf95bc8d7068685dedf074
Benedict Nkosi <benedict@kineticskunk.com>
2021-07-24 22:08:30 +0200

    Now you can click on a calendar item

--- js/operations/calendar.js
+++ js/operations/calendar.js
@@ -7,7 +7,23 @@ $(document).ready(function() {
 
 function getcalendar(period) {
 	$("#calendar-table").load("operations/reservations/getcalendar.php", function() {
+		$(".image_checkin").click(function(event) {
+			jumpToBooking(event);
+		});
 	});
 
 }
+
+
+function jumpToBooking(event) {
+	$reservation_id= event.target.id.replace("img_calendar_", "");
+	updateView("upcoming-reservations");
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $("a:contains('" +$reservation_id +"')").offset().top
+    }, 2000);
+	$("a:contains('" +$reservation_id +"')").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
+	
+}
+
+
 
