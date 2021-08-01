a5209759e8515af89ad9bedf5624706581299545
Benedict <benedict.nkosi01@dixonscarphone.com>
2021-08-01 19:32:03 +0200

    added a page to delete old invoices

--- operations/app/application.php
+++ operations/app/application.php
@@ -5,4 +5,6 @@ define("COMPANY_PHONE_NUMBER", "+27796347610");
 define("MANAGER_PHONE_NUMBER", "+27837917430");
 define("BULK_SMS_URL", "https://api.bulksms.com/v1/messages?auto-unicode=true&longMessageMaxParts=30");
 define("EMAIL_ADDRESS", "info@aluvegh.co.za");
+define("INVOICES_FOLDER", "/invoices");
+define("INVOICE_RETENTION_DAYS", 180);
 ?> 
