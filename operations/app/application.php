44035c88979efc6359b153441ae0db1a65bf5bf1
Benedict <benedict.nkosi01@dixonscarphone.com>
2021-08-02 22:50:06 +0200

    UPDATES SMS LOGIN

--- operations/app/application.php
+++ operations/app/application.php
@@ -1,8 +1,9 @@
 <?php
-define("USERNAME", "aluvegh");
-define("PASSWORD", "Nhlaka@03");
+define("USERNAME", "aluve_bulksms");
+define("PASSWORD", "AIupCrPzleo1");
 define("COMPANY_PHONE_NUMBER", "+27796347610");
 define("MANAGER_PHONE_NUMBER", "+27837917430");
+
 define("BULK_SMS_URL", "https://api.bulksms.com/v1/messages?auto-unicode=true&longMessageMaxParts=30");
 define("EMAIL_ADDRESS", "info@aluvegh.co.za");
 define("EMAIL_PASSWORD", "Nhlaka@02");
