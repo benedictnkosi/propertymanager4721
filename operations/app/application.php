d23af4a3fb49c22951c6c65ead0c02c9525b840b
benedictnkosi <nkosi.benedict@gmail.com>
2021-08-15 11:52:59 +0200

    Update application.php

--- operations/app/application.php
+++ operations/app/application.php
@@ -6,7 +6,7 @@ define("MANAGER_PHONE_NUMBER", "+27837917430");
 
 define("BULK_SMS_URL", "https://api.bulksms.com/v1/messages?auto-unicode=true&longMessageMaxParts=30");
 define("EMAIL_ADDRESS", "info@aluvegh.co.za");
-define("EMAIL_PASSWORD", "Nhlaka@02");
+define("EMAIL_PASSWORD", "jX{Kdc^SDFyz");
 define("EMAIL_SERVER", "mail.aluvegh.co.za");
 define("EMAIL_SERVER_PORT", "995");
 define("INVOICES_FOLDER", "/invoices");
