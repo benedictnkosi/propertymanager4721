d11b1ccc88a63f8cb924e6b6d72fca0d972dc715
benedictnkosi <nkosi.benedict@gmail.com>
2022-05-01 23:57:04 +0200

    Update block.php

--- operations/customer/block.php
+++ operations/customer/block.php
@@ -8,5 +8,9 @@ if (isset($_POST["customer_id"])) {
 
 function blockCustomer($customerId)
 {
-   
+   $return_array = array();
+
+    $sqlUpdateRes = "update wpky_hb_customers set state =  'blocked', comments = ' " . $comments .'" where id = " . $customerId;
+
+    $resultCreateRes = updaterecord($sqlUpdateRes);
 }
