eede8cd10fc88cf541047b6665bc5795c502f1f7
benedictnkosi <nkosi.benedict@gmail.com>
2022-05-01 23:56:36 +0200

    Update block.php

--- operations/customer/block.php
+++ operations/customer/block.php
@@ -8,22 +8,5 @@ if (isset($_POST["customer_id"])) {
 
 function blockCustomer($customerId)
 {
-    $return_array = array();
-
-    $sqlUpdateRes = "update wpky_hb_customers set state =  'blocked', comments = ' " . $comments .'" where id = " . $customerId;
-
-    $resultCreateRes = updaterecord($sqlUpdateRes);
-        if (strcasecmp($resultCreateRes, "Record updated successfully") == 0) {
-            $temparray1 = array(
-                'result_code' => 0,
-                'result_desciption' => "Customer successfully blocked"
-            );
-            echo json_encode($temparray1);
-        } else {
-            $temparray1 = array(
-                'result_code' => 0,
-                'result_desciption' => "Failed to block customer"
-            );
-            echo json_encode($temparray1);
-        }
+   
 }
