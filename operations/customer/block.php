a9f4dfa296cdffe301d20a7426bcd387dc75155f
benedictnkosi <nkosi.benedict@gmail.com>
2022-05-01 23:51:30 +0200

    Update block.php

--- operations/customer/block.php
+++ operations/customer/block.php
@@ -4,32 +4,4 @@ require_once (__DIR__ . '/../utils/sms.php');
 require_once (__DIR__ . '/commons.php');
 require_once (__DIR__ . '/../app/application.php');
 
-
-if (isset($_POST["customer_id"])) {
-    blockCustomer($_POST["customer_id"]);
-}else{
-    echo 'shit';
-}
-
-
-function blockCustomer($customerId)
-{
-    $return_array = array();
-
-    $sqlUpdateRes = "update wpky_hb_customers set state =  'blocked', comments = ' " . $comments .'" where id = " . $customerId;
-
-    $resultCreateRes = updaterecord($sqlUpdateRes);
-        if (strcasecmp($resultCreateRes, "Record updated successfully") == 0) {
-            $temparray1 = array(
-                'result_code' => 0,
-                'result_desciption' => "Customer successfully blocked"
-            );
-            echo json_encode($temparray1);
-        } else {
-            $temparray1 = array(
-                'result_code' => 0,
-                'result_desciption' => "Failed to block customer"
-            );
-            echo json_encode($temparray1);
-        }
-}
+echo 'hello';
