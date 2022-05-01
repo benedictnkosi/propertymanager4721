e8af2b22980857436f76753f3bb14b697a391a3f
benedictnkosi <nkosi.benedict@gmail.com>
2022-05-01 23:54:13 +0200

    Update block.php

--- operations/customer/block.php
+++ operations/customer/block.php
@@ -1,7 +1,3 @@
 <?php
-require_once (__DIR__ . '/../utils/data.php');
-require_once (__DIR__ . '/../utils/sms.php');
-require_once (__DIR__ . '/commons.php');
-require_once (__DIR__ . '/../app/application.php');
 
 echo 'hello';
